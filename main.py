5ad2039a81611203a435fecd4419f2ebf51d7126
Sam Davies <samdavies@live.co.uk>
2015-02-17 22:29:11 +0000

    found the bounding box!

--- main.py
+++ main.py
@@ -4,18 +4,66 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('Images/ivr1415pract1data1/train3.jpg', 0)
-ret, thresh1 = cv2.threshold(img, 129, 255, cv2.THRESH_BINARY)
-ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
-ret, thresh3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
-ret, thresh4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
-ret, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
 
-titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
-images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
 
-for i in xrange(6):
-    plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
-    plt.title(titles[i])
-    plt.xticks([]), plt.yticks([])
+def threshes_example():
+    ret, thresh1 = cv2.threshold(img, 129, 255, cv2.THRESH_BINARY)
+    ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+    ret, thresh3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
+    ret, thresh4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+    ret, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
 
-plt.show()
+    titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+    images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+
+    for i in xrange(6):
+        plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
+        plt.title(titles[i])
+        plt.xticks([]), plt.yticks([])
+
+    plt.show()
+
+
+def contour_example():
+    ret, thresh = cv2.threshold(img, 129, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    cnt = contours[4]
+    print "{0} contours found".format(len(contours))
+    cv2.drawContours(img, [cnt], 0, (0, 255, 0), 3)
+    plt.imshow(img)
+    plt.title("Contour 4")
+    plt.show()
+
+
+def bounding_box_example():
+    # turn the image into binary (black and white, no grey)
+    ret, thresh = cv2.threshold(img, 129, 255, cv2.THRESH_BINARY)
+    # find all the contours in the image, all areas of joint white/black
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # figure out the largest contour area
+    biggest_cnt = None
+    biggest_cnt_area = 0
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print area
+        if area > biggest_cnt_area:
+            biggest_cnt = cnt
+            biggest_cnt_area = area
+
+    # draw a rotated box around the biggest contour
+    rect = cv2.minAreaRect(biggest_cnt)
+    box = cv2.cv.BoxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+
+    # display the image
+    plt.imshow(img)
+    plt.title("bounding box")
+    plt.show()
+
+
+if __name__ == "__main__":
+    bounding_box_example()
